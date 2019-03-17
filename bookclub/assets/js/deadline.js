fec7735da579e1349df76f728b99fde703224152
hejazizo <hejazizo@ualberta.ca>
2019-03-17 14:55:25 -0600

    updated new book

--- bookclub/assets/js/deadline.js
+++ bookclub/assets/js/deadline.js
@@ -1,8 +1,11 @@
 ﻿// Set the date we're counting down to
-var countDownDate = new Date("APR 06, 2019 17:00:00").getTime();
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
+
+    var next_date = document.getElementById("next_date").innerHTML;
+    var countDownDate = new Date(next_date  ).getTime();
 
     // Get todays date and time
     var now = new Date().getTime();
@@ -19,12 +22,12 @@ var x = setInterval(function() {
     // Output the result in an element with id="deadline"
     document.getElementById("deadline").innerHTML = " " + days + "d " + hours + "h " +
         minutes + "m " + seconds + "s ";
-    document.getElementById("deadline").style.color = "GREEN";
+    document.getElementById("deadline").style.color = "#DC143C";
 
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("deadline").innerHTML = " FINISHED.";
+        document.getElementById("next_book").style.display = "none";
         document.getElementById("deadline").style.color = "GREEN";
     }
 }, 1000);
